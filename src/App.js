c0feb99629d320c6d72acdddfe70b40a4a9f5796
TeslaSeven7 <alex.aubrier@epitech.eu>
2022-08-07 20:42:41 +0200

    Added URL checking and user based text generation

--- src/App.js
+++ src/App.js
@@ -6,48 +6,64 @@ const url = "https://api.shrtco.de/v2/shorten?url=";
 var longUrl;
 var regex= /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
 export default function App() {
-
+  
   const [message, setMessage] = useState('');
+  const [messageTwo, setMessageTwo] = useState('');
   const handleChange = event => {
-    if(regex.test(event.target.value)){
-      longUrl = url + event.target.value
-      
+    if (regex.test(event.target.value)){
+      longUrl = url + event.target.value;
     }
     else{
-      console.log("NOPE")
+    setMessageTwo('NOT VALID URL');
+      
     }
     setMessage(event.target.value);
   };
-
-
+  
+  
   const [isSending, setIsSending] = useState(false);
-  let [data, setData] = useState([]);
-
+  const [data, setData] = useState([]);
   useEffect(() => {
-    isSending &&
+    if(isSending && typeof longUrl  !== "undefined"){
+      
       fetch(longUrl)
-        .then((response) => response.json())
-        .then((data) => setData(data.result.full_short_link))
-        .then(() => setIsSending(false));
+      .then(data => {
+        if (data.ok) {
+          return data.json()
+        } else if(data.status === 400) {
+          return Promise.reject('error 400')
+        } else {
+          return Promise.reject('some other error: ' + data.status)
+        }
+      }) 
+      .then((data) => setData(data.result.full_short_link))
+      .then(() => setIsSending(false));
+      setMessageTwo('wink')
+      setIsSending(false);
+    }
+    setIsSending(false);
+    
+    
   }, [isSending]);
-
+  
   return (
     <>
-     <div>
-        <input
-        type="text"
-        id="message"
-        name="message"
-        onChange={handleChange}
-        value={message}
-        />
-        
-        <h2>Message: {message}</h2>
-        
-        </div>
+    <div>
+    <input
+    type="text"
+    id="message"
+    name="message"
+    onChange={handleChange}
+    value={message}
+    />
+    
+    <h2>Message: {messageTwo}</h2>
+    
+    </div>
     <button onClick={() => setIsSending(true)}>Generate URL</button>
-      <h2><a href={data} target="_blank">{data}</a></h2>
-      
+    <h2><a href={data} target="_blank">{data}</a></h2>
+    
     </>
-  );
-}
+    );
+  }
+  
